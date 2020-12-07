9d0262c8ba6058c705735d6baf564d1a96cb3d68
Tristan Endreo <endreotm@gmail.com>
2020-12-06 19:52:54 -0800

    Remove Identity files

---
+++ Server/Services/ChatGroupService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LearnBlazor.Server.Data.Models;
+
+namespace LearnBlazor.Server.Services
+{
+    public class ChatGroupService
+    {
+    }
+}
